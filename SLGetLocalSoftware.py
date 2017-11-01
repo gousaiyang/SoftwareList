ec9d8e9de7dccaf237c31be3c2bf255b753b78c7
gousaiyang <gousaiyang@163.com>
2017-11-01 20:07:24 +0800

    fix missing per-user software

--- SLGetLocalSoftware.py
+++ SLGetLocalSoftware.py
@@ -8,8 +8,8 @@ def exec_powershell(command):
     p = Popen(['powershell', command], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     return p.communicate()[0]
 
-def get_name_and_version(content):
-    for s in content.strip().split('\r\n\r\n'):
+def parse_ps_output(content):
+    for s in content.decode('gbk').strip().split('\r\n\r\n'):
         r = re.findall(r'DisplayName(?:\s*):(?:\s*)(.*)\r\nDisplayVersion(?:\s*):(?:\s*)(.*)', s)
         if r:
             yield (r[0][0].strip(), r[0][1].strip())
@@ -23,8 +23,9 @@ def get_local_software():
 
     list1_b = exec_powershell('Get-ItemProperty HKLM:\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Format-List DisplayName, DisplayVersion | out-string -Width 2000')
     list2_b = exec_powershell('Get-ItemProperty HKLM:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Format-List DisplayName, DisplayVersion | out-string -Width 2000')
+    list3_b = exec_powershell('Get-ItemProperty HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Format-List DisplayName, DisplayVersion | out-string -Width 2000')
 
-    for n, v in itertools.chain(get_name_and_version(list1_b.decode('gbk')), get_name_and_version(list2_b.decode('gbk'))):
+    for n, v in itertools.chain(parse_ps_output(list1_b), parse_ps_output(list2_b), parse_ps_output(list3_b)):
         software_list[n] = v
 
     return software_list
