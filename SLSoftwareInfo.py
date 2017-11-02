430ac35e928b2ef886f7db11d6b1abb0ff2c9f72
gousaiyang <gousaiyang@163.com>
2017-11-02 15:09:15 +0800

    enrich software info

--- SLSoftwareInfo.py
+++ SLSoftwareInfo.py
@@ -1,8 +1,81 @@
 # -*- coding: utf-8 -*-
 
-# TODO: Enrich this list with more software.
-
 software_info = {
+    'Atom': {
+        'DownloadURL': 'https://atom.io/',
+        'CheckUpdateURL': 'https://github.com/atom/atom/releases.atom',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'Atom'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'/atom/atom/releases/tag/v([\d.]+)"'
+        }
+    },
+    'Beyond Compare': {
+        'DownloadURL': 'https://www.scootersoftware.com/download.php',
+        'CheckUpdateURL': 'https://www.scootersoftware.com/download.php',
+        'CurrentVersionDetection': {
+            'Type': 'InName',
+            'Selector': r'Beyond Compare ([\d.]+)'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'Current Version:(?:&nbsp;|\s)*([\d.]+)'
+        },
+        'ReleaseDateDetection': {
+            'Type': 'Regex',
+            'Selector': r'released(.*)</p>'
+        }
+    },
+    'Dev-C++': {
+        'DownloadURL': 'https://sourceforge.net/projects/orwelldevcpp/',
+        'CheckUpdateURL': 'https://sourceforge.net/projects/orwelldevcpp/',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'Dev-C\+\+'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'Dev-Cpp ([\d.]+)'
+        },
+        'ReleaseDateDetection': {
+            'Type': 'BeautifulSoup',
+            'Selector': 'time[class="dateUpdated"]'
+        }
+    },
+    'FastStone Capture': {
+        'DownloadURL': 'http://www.faststone.org/FSCapturerDownload.htm',
+        'CheckUpdateURL': 'http://www.faststone.org/FSCapturerDownload.htm',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'FastStone Capture'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'FastStone Capture ([\d.]+)'
+        },
+        'ReleaseDateDetection': {
+            'Type': 'Regex',
+            'Selector': r'\d{4}-\d{2}-\d{2}'
+        }
+    },
+    'Fiddler': {
+        'DownloadURL': 'https://www.telerik.com/download/fiddler'
+    },
+    'foobar2000': {
+        'DownloadURL': 'https://www.foobar2000.org/download',
+        'CheckUpdateURL': 'https://www.foobar2000.org/download',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'foobar2000'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'foobar2000 v([\d.]+)'
+        }
+    },
     'Git': {
         'DownloadURL': 'https://git-scm.com/',
         'CheckUpdateURL': 'https://git-scm.com/',
@@ -19,6 +92,89 @@ software_info = {
             'Selector': 'span[class="release-date"]'
         }
     },
+    'Google Chrome': {
+        'DownloadURL': 'https://www.google.cn/chrome/browser/desktop/index.html?standalone=1',
+        'CheckUpdateURL': 'https://omahaproxy.appspot.com/all?csv=1',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'Google Chrome'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'win64,stable,([\d.]+)'
+        }
+    },
+    'Java 8': {
+        'DownloadURL': 'https://www.java.com/zh_CN/download/',
+        'CheckUpdateURL': 'https://www.java.com/zh_CN/download/',
+        'CurrentVersionDetection': {
+            'Type': 'InName',
+            'Selector': r'Java 8 Update (\d+)'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'Version 8 Update (\d+)'
+        },
+        'ReleaseDateDetection': {
+            'Type': 'Regex',
+            'Selector': r'<meta name="date" content="([^"]*)"/>'
+        }
+    },
+    'MagicEXIF Metadata Editor': {
+        'DownloadURL': 'http://www.magicexif.com/',
+        'CheckUpdateURL': 'http://www.magicexif.com/update/update_info.xml',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'MagicEXIF Metadata Editor'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'<Version>([^<>]*)</Version>'
+        }
+    },
+    'MarkdownPad': {
+        'DownloadURL': 'http://markdownpad.com/'
+    },
+    'Node.js': {
+        'DownloadURL': 'https://nodejs.org/zh-cn/',
+        'CheckUpdateURL': 'https://nodejs.org/zh-cn/download/',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'Node\.js'
+        },
+        'NewestVersionDetection': {
+            'Type': 'BeautifulSoup',
+            'Selector': 'strong'
+        }
+    },
+    'Oracle VM VirtualBox': {
+        'DownloadURL': 'https://www.virtualbox.org/wiki/Downloads',
+        'CheckUpdateURL': 'https://www.virtualbox.org/wiki/Downloads',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'Oracle VM VirtualBox'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'VirtualBox ([\d.]+) platform packages'
+        }
+    },
+    'Postman': {
+        'DownloadURL': 'https://www.getpostman.com/',
+        'CheckUpdateURL': 'https://dl.pstmn.io/changelog?channel=stable&platform=win',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'Postman'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'https://dl\.pstmn\.io/download/version/([\d.]+)'
+        },
+        'ReleaseDateDetection': {
+            'Type': 'Regex',
+            'Selector': r'"createdAt":"(\d{4}-\d{2}-\d{2})'
+        }
+    },
     'Python 3': {
         'DownloadURL': 'https://www.python.org/downloads/',
         'CheckUpdateURL': 'https://www.python.org/',
@@ -29,16 +185,141 @@ software_info = {
         'NewestVersionDetection': {
             'Type': 'Regex',
             'Selector': r'Latest: <a href="[^"]*">Python (3\.[\d.]+)</a>'
-        },
-    },
-    'QQ': {
-        'DownloadURL': 'https://im.qq.com/',
-        'CurrentVersionDetection': {
-            'Type': 'InVersion',
-            'Selector': r'腾讯QQ'
         }
     },
-    'Wechat': {
+    'Sublime Text': {
+        'DownloadURL': 'https://www.sublimetext.com/',
+        'CheckUpdateURL': 'https://www.sublimetext.com/',
+        'CurrentVersionDetection': {
+            'Type': 'InName',
+            'Selector': r'Sublime Text Build (\d+)'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'Build (\d+)'
+        }
+    },
+    'TeamViewer': {
+        'DownloadURL': 'https://www.teamviewer.com/zhcn/download/windows/',
+        'CheckUpdateURL': 'https://www.teamviewer.com/zhcn/download/windows/',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'TeamViewer'
+        },
+        'NewestVersionDetection': {
+            'Type': 'BeautifulSoupWithRegex',
+            'bs': 'p[class="DownloadVersion"]',
+            're': r'v([\d.]+)'
+        }
+    },
+    'WinRAR': {
+        'DownloadURL': 'https://www.win-rar.com/download.html?&L=0',
+        'CheckUpdateURL': 'https://www.win-rar.com/download.html?&L=0',
+        'CurrentVersionDetection': {
+            'Type': 'InName',
+            'Selector': r'WinRAR ([\d.]+)'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'WinRAR ([\d.]+)'
+        }
+    },
+    'Wireshark': {
+        'DownloadURL': 'https://www.wireshark.org/download.html',
+        'CheckUpdateURL': 'https://www.wireshark.org/download.html',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'Wireshark'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'>Stable Release \(([\d.]+)\)'
+        }
+    },
+    'Yarn': {
+        'DownloadURL': 'https://yarnpkg.com/zh-Hans/',
+        'CheckUpdateURL': 'https://yarnpkg.com/zh-Hans/',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'Yarn'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'https://github\.com/yarnpkg/yarn/releases/tag/v([\d.]+)'
+        }
+    },
+    '微信': {
         'DownloadURL': 'https://pc.weixin.qq.com/'
+    },
+    '搜狗拼音输入法': {
+        'DownloadURL': 'http://pinyin.sogou.com/',
+        'CheckUpdateURL': 'http://pinyin.sogou.com/',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'搜狗拼音输入法'
+        },
+        'NewestVersionDetection': {
+            'Type': 'BeautifulSoup',
+            'Selector': 'version'
+        },
+        'ReleaseDateDetection': {
+            'Type': 'Regex',
+            'Selector': r'更新日期(?:\s*):(?:\s*)([\d-]+)'
+        }
+    },
+    '暴风影音': {
+        'DownloadURL': 'http://home.baofeng.com/',
+        'CheckUpdateURL': 'http://home.baofeng.com/',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'暴风影音'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'([\d.]+)<a'
+        }
+    },
+    '百度网盘': {
+        'DownloadURL': 'https://pan.baidu.com/download',
+        'CheckUpdateURL': 'https://pan.baidu.com/disk/cmsdata?do=client',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'百度网盘'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'百度网盘PC版 V([\d.]+)'
+        }
+    },
+    '福昕阅读器': {
+        'DownloadURL': 'https://www.foxitsoftware.cn/downloads/',
+        'CheckUpdateURL': 'https://www.foxitsoftware.cn/downloads/require/getPackage.php?product=Foxit-Reader&language=Chinese&platform=Windows',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'福昕阅读器'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'"version":\["([\d.]+)"\]'
+        }
+    },
+    '福昕PDF编辑器': {
+        'DownloadURL': 'http://edit.foxitreader.cn/',
+        'CheckUpdateURL': 'http://m.edit.foxitreader.cn/api/getWebExeDownload',
+        'CurrentVersionDetection': {
+            'Type': 'InVersion',
+            'Selector': r'福昕PDF编辑器'
+        },
+        'NewestVersionDetection': {
+            'Type': 'Regex',
+            'Selector': r'"installVersion":"([\d.]+)"'
+        },
+        'ReleaseDateDetection': {
+            'Type': 'Regex',
+            'Selector': r'"updateTime":"([^"]*)"'
+        }
+    },
+    '腾讯QQ': {
+        'DownloadURL': 'https://im.qq.com/pcqq/'
     }
 }
