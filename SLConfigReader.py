6cb403a7cc9a6ccf09a3330a29b2506f88cafd0a
gousaiyang <gousaiyang@163.com>
2018-12-28 22:48:49 +0800

    Do not use assert for validation

--- SLConfigReader.py
+++ SLConfigReader.py
@@ -16,7 +16,9 @@ def get_theme_config():
     try:
         data = json.loads(file_content('Config.json'))
         theme = data['theme'].lower()
-        assert theme == 'random' or theme in all_themes
+
+        if theme not in all_themes and theme != 'random':
+            raise ValueError('invalid theme')
     except Exception:
         theme = default_theme.lower()
 
@@ -30,7 +32,10 @@ def get_adb_config():
     try:
         data = json.loads(file_content('Config.json'))
         adb_exec = data['adb_exec']
-        assert isinstance(adb_exec, str)
+
+        if not isinstance(adb_exec, str):
+            raise TypeError("'adb_exec' should be str")
+
         return adb_exec
     except Exception:
         return None
@@ -40,7 +45,10 @@ def get_aapt_config():
     try:
         data = json.loads(file_content('Config.json'))
         aapt_exec = data['aapt_exec']
-        assert isinstance(aapt_exec, str)
+
+        if not isinstance(aapt_exec, str):
+            raise TypeError("'aapt_exec' should be str")
+
         return aapt_exec
     except Exception:
         return None
